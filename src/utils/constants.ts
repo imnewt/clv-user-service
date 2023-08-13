c895469737e326fd35a6757bf9baf42191c89d31
imnewtt <51883259+imnewt@users.noreply.github.com>
2023-08-13 23:12:01 +0700

    setup kafka

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,7 @@ export const GATEWAY_PORT = 9001;
 export const USER_SERVICE_PORT = 9002;
 
 export const GATEWAY_HOST_URL = `http://localhost:${GATEWAY_PORT}`;
+
+export const KAFKA_BROKER_ADDRESS = 'http://localhost:9092';
+export const KAFKA_CLIENT_ID = 'user-service-app';
+export const KAFKA_GROUP_ID = 'user-service-group';
