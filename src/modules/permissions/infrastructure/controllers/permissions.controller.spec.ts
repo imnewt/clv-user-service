7d3860e4401c3b51947cbb3ce641f59da7ea6eaf
imnewtt <51883259+imnewt@users.noreply.github.com>
2023-09-01 13:29:34 +0700

    unit tests for permission module

--- src/modules/permissions/infrastructure/controllers/permissions.controller.spec.ts
+++ src/modules/permissions/infrastructure/controllers/permissions.controller.spec.ts
@@ -1,19 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
 
 import { PermissionsController } from './permissions.controller';
+import { PermissionsService } from '@permissions/services/permissions.service';
+import { UsersService } from '@users/services/users.service';
+import { RolesService } from '@roles/services/roles.service';
+import { FilterDto } from '@shared/dtos/filter.dto';
+import { User, Role, Permission } from '@shared/entities';
+import { jwtConfig } from '@shared/configs/jwtConfig';
 
 describe('PermissionsController', () => {
-  let controller: PermissionsController;
+  let permissionsController: PermissionsController;
+  let permissionRepository: Repository<Permission>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule.register(jwtConfig)],
       controllers: [PermissionsController],
+      providers: [
+        UsersService,
+        RolesService,
+        PermissionsService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Role),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Permission),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<PermissionsController>(PermissionsController);
+    permissionsController = module.get<PermissionsController>(
+      PermissionsController,
+    );
+    permissionRepository = module.get<Repository<Permission>>(
+      getRepositoryToken(Permission),
+    );
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getAllPermissions', () => {
+    it('should return an array of permissions and total count', async () => {
+      const filter: FilterDto = {
+        searchTerm: 'example',
+        pageNumber: 1,
+        pageSize: 10,
+      };
+      const permissions = [new Permission(), new Permission()];
+      const total = 2;
+
+      permissionRepository.findAndCount = jest
+        .fn()
+        .mockResolvedValue([permissions, total]);
+
+      const result = await permissionsController.getPermissions(filter);
+
+      expect(result.permissions).toBeInstanceOf(Array);
+      expect(result.permissions.length).toBe(permissions.length);
+      expect(result.total).toBe(total);
+    });
   });
 });
--- src/modules/permissions/services/permissions.service.spec.ts
+++ src/modules/permissions/services/permissions.service.spec.ts
@@ -1,19 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import { PermissionsService } from './permissions.service';
+import { Permission } from '@shared/entities';
+import { FilterDto } from '@shared/dtos/filter.dto';
 
 describe('PermissionsService', () => {
-  let service: PermissionsService;
+  let permissionsService: PermissionsService;
+  let permissionRepository: Repository<Permission>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PermissionsService],
+      providers: [
+        PermissionsService,
+        {
+          provide: getRepositoryToken(Permission),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    service = module.get<PermissionsService>(PermissionsService);
+    permissionsService = module.get<PermissionsService>(PermissionsService);
+    permissionRepository = module.get<Repository<Permission>>(
+      getRepositoryToken(Permission),
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getAllPermissions', () => {
+    it('should return an array of permissions and total count', async () => {
+      const filter: FilterDto = {
+        searchTerm: 'example',
+        pageNumber: 1,
+        pageSize: 10,
+      };
+      const permissions = [new Permission(), new Permission()];
+      const total = 2;
+
+      permissionRepository.findAndCount = jest
+        .fn()
+        .mockResolvedValue([permissions, total]);
+
+      const result = await permissionsService.getPermissions(filter);
+
+      expect(result.permissions).toBeInstanceOf(Array);
+      expect(result.permissions.length).toBe(permissions.length);
+      expect(result.total).toBe(total);
+    });
+  });
+
+  describe('getPermissionsByIds', () => {
+    it('should return permissions by ids', async () => {
+      const permissionIds = ['permission_id_1', 'permission_id_2'];
+      const permissions = [new Permission(), new Permission()];
+      permissionRepository.find = jest.fn().mockResolvedValue(permissions);
+
+      const result = await permissionsService.getPermissionsByIds(
+        permissionIds,
+      );
+
+      expect(result).toBe(permissions);
+    });
   });
 });
